3dfde63013aed6fd0fb9119e7b29816c448fc577
JCamyre <JWcamry03@gmail.com>
2021-09-09 19:09:47 -0700

    Display username and profile pic

--- frontend/src/FetchApi.jsx
+++ frontend/src/FetchApi.jsx
@@ -14,29 +14,38 @@ function GetApi(){
     .then((response) => {
         if(response.data){
             // console.log(JSON.stringify(response));
-            return JSON.stringify(response);
+            // return JSON.stringify(response, null, 2); // idk what null, 2 does.
+            return response;
         }
     });
 // Don't use useEffect if you are using a separate function for getting API?
 // Huge brain move: use the useEffect when you are calling this function in the main component (FetchApi). 
 }
 
+// Display username and profile pic
+// Could split up into more files, but it find
 function FetchApi() {
-    const [data, setData] = useState('');
+    // const [data, setData] = useState('');
+    const [name, setName] = useState('');
+    const [profileImg, setProfileImg] = useState('');
 
-    // Best to put the API getting logic in a separate file (like an API components folder)
+    // Best to put the API getting logic (GetApi) in a separate file (like an API components folder) separate from this FetchApi.jsx file
     useEffect(() => {
         GetApi().then((result) => {
-            setData(result);
+            // setData(result);
+            if (result) {
+                console.log(result['data']['results'][0]['picture']['medium']);
+                setName(result['data']['results'][0]['name']['first'] + ' ' + result['data']['results'][0]['name']['last']);
+                setProfileImg(result['data']['results'][0]['picture']['large'])
+            }
         })
     }, []); // [data] doesn't work here since it checks the "next" value to the current value and since this api returns random data every time, always update, so we just have it empty so it will only load once (on page load). 
 
 
-    console.log(data);
-
     return (
         <div>
-            <h3>{data}</h3>
+            <img alt='Profile pic' src={profileImg} />
+            <h2>{name}</h2>
         </div>
     )
 }
