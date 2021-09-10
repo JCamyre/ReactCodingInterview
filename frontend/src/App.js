3b33f0dae4ca3e2ce1a65efef33b5cd780a9bc9c
JCamyre <JWcamry03@gmail.com>
2021-09-09 17:13:22 -0700

    Add button counter

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonCounter from "./ButtonCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonCounter />
+    </div>
+  );
+}
+
+export default App;
