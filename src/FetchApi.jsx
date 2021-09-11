d57b1dc3db1f56bcb7c54dd0f22a7319a48f5489
JCamyre <JWcamry03@gmail.com>
2021-09-10 21:29:02 -0700

    Try to solve bug

--- src/FetchApi.jsx
+++ src/FetchApi.jsx
@@ -11,15 +11,15 @@ function GetApi(page){
     //?page=2, page=3, ...
     // You return the get function, and then inside the function you return the JSON. Don't forget for axios two returns. 
     return axios.get(`https://randomuser.me/api?page=${page}`)
-    .catch((err) => {
-        console.error('There has been an error');
-    })
     .then((response) => {
-        if(response.data){
+        if(response){
             // console.log(JSON.stringify(response));
             // return JSON.stringify(response, null, 2); // idk what null, 2 does.
             return response;
         }
+    })
+    .catch((err) => {
+        console.error('There has been an error');
     });
 // Don't use useEffect if you are using a separate function for getting API?
 // Huge brain move: use the useEffect when you are calling this function in the main component (FetchApi). 
@@ -29,34 +29,18 @@ function GetApi(page){
 // Could split up into more files, but it find
 function FetchApi() {
     const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
     const [page, setPage] = useState(0);
 
     // Best to put the API getting logic (GetApi) in a separate file (like an API components folder) separate from this FetchApi.jsx file
     useEffect(() => {
-        // var temp = [];
-        // for loop -> make a call each time, add to temp, after loop, setUsers(temp)
-        // for(let i=0;i<10;i++) {
-        //     GetApi().then((result) => {
-        //         // setData(result);
-        //         if (result) {
-        //             // could do a dictionary. name: result['name']
-        //             temp.push(result['data']['results']);
-        //         }
-        //     }); 
-        // }
-        GetApi(page).then((result) => {
-            if (result) {
-                setUsers(result['data'])
-            }
+        GetApi(page)
+        .then((result) => {
+            setUsers(result);
         })
-        // setUsers(temp);
-        setIsLoading(false);
-        // Given GetApi() returns a list of 'data', I'll just act on element
 
     }, []); // [data] doesn't work here since it checks the "next" value to the current value and since this api returns random data every time, always update, so we just have it empty so it will only load once (on page load). 
 
-
+    console.log(typeof users, typeof []);
 
     const getFullUserName = (user) => {
         // This is some cool code: Set the variables first, last equal to user['name'], thanks to the {name: }. 
@@ -67,24 +51,27 @@ function FetchApi() {
 
     const loadNextUser = (pagenum, users) => {
         users.push(GetApi(pagenum));
+        setUsers(users);
         setPage(pagenum + 1);
     };
 
     return (
         <div>
-            {/* {['a', 'b', 'c'].map((element) => (
-                <h2>{element}</h2>
-            ))} */}
-            {users.map((user, index) => {
-                console.log(user, index);
-            })}
 
-            {users.map((user, index) => (
-                <div key={index}>
-                    {/* <h3>{getFullUserName(user)}</h3> */}
-                    <h1>{user['name']}</h1>
-                </div>
-            ))}
+            {
+                users.map((user, index) => {
+                    console.log(user);
+                })
+            }
+
+            {
+                users.map((user, index) => (
+                    <div key={index}>
+                        {/* <h3>{getFullUserName(user)}</h3> */}
+                        <h1>{user['name']}</h1>
+                    </div>
+                ))
+            }
             <button onClick={() => {
                 loadNextUser(page, users);
             }}>
