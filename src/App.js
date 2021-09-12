e11002eee2de3010d5c6c61cfed47a86fdaedb8d
JCamyre <JWcamry03@gmail.com>
2021-09-12 13:12:33 -0700

    Work on problem one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ButtonCounter from "./ButtonCounter";
-import FetchApi from "./FetchApi.jsx";
+// import ButtonCounter from "./Easy/ButtonCounter";
+// import FetchApi from "./Easy/FetchApi.jsx";
+import FetchApi from "./Medium/FetchApi";
 
 function App() {
   return (
     <div className="App">
-      <ButtonCounter />
       <FetchApi />
     </div>
   );
